55b0e8909438e95dae9d0447a8bd6e9c76e56683
JeevanJoshi4434 <jeetbhaiya25@gmail.com>
2024-02-23 00:31:34 +0530

    Added Stats

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da1ed39565a3796eb587952fac6e5b41.js"
+  "/precache-manifest.a3bbd291f7a5d5c039c67dc1d6b4b56d.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ build/static/js/22.7cccc715.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[22],{1502:function(a,s,d){}}]);
+//# sourceMappingURL=22.7cccc715.chunk.js.map
